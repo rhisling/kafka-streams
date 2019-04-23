62bb3b8884b39165bfa2cf23677dd70adcd279f7
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-04-22 19:03:48 -0700

    Adding target dir

--- wordcount/src/main/java/me/aravindh/App.java
+++ wordcount/src/main/java/me/aravindh/App.java
@@ -3,8 +3,13 @@ package me.aravindh;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class App {
@@ -12,8 +17,41 @@ public class App {
         Properties properties = new Properties();
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-starter-app");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "-app");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+
+        // 1. Stream from Kafka
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+
+        // 2. map values to lowercase
+        KTable<String, Long> wordCounts = wordCountInput.mapValues(String::toLowerCase)
+
+                // 3. flatmap values split by space
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+
+                // 4. Select key to apply a key (we discard old key)
+                .selectKey((key, value) -> value)
+
+                // 5.   group by key before aggregation
+                .groupByKey()
+
+                // 6. count occurrences
+                .count("Counts");
+
+
+        // 7. write result back to kafka
+        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
+
+        KafkaStreams streams = new KafkaStreams(builder, properties);
+        streams.start();
+
+        //print the topology
+        System.out.println(streams.toString());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
     }
 }
